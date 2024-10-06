66490d43d77b62376dc1fc5a4d1727792f545c2a
Krisna Bei <krisnaabei28@gmail.com>
2024-10-06 17:16:39 +0700

    home

---
+++ code/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem RAKPP | {{ $title }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
+</head>
+
+
+<body class="{{ strtolower($title) }}">
+    @include('partials.navbar')
+
+    @yield('container')
+</body>
+</html>
---
+++ code/partials/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #2C3E50; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);">
+    <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+            <img src="img/logo.png" alt="Logo" class="animate__animated animate__fadeIn animate__delay-1s" style="max-height: 50px; transform: scale(1.1);">
+            <span class="navbar-brand text-white">SPK Kontrasepsi</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
