570c9c286220438c9f456caa52feecb80dcc648f
Krisna Bei <krisnaabei28@gmail.com>
2024-10-20 18:11:02 +0700

    data baru

--- code/webb.php
+++ code/webb.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 
 //Homepage
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/login', [HomeController::class, 'indexLogin']);
-Route::get('/registration', [HomeController::class, 'indexRegistration']);
 Route::get('/beranda', [HomeController::class, 'indexBeranda']);
+
+//Login
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+//Register
+Route::get('/registration', [RegisterController::class, 'create']);
+Route::post('/registration', [RegisterController::class, 'store']);
